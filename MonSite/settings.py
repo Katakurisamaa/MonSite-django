7a08073ceb58b9157ef0a76454abd41838775bcb
Katakurisamaa <benjamin.minko@student.uclouvain.be>
2022-10-10 13:27:39 +0200

    changement

--- MonSite/settings.py
+++ MonSite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['sosorelooking-env.eba-m3rcbnwp.eu-west-3.elasticbeanstalk.com']
 
 
 # Application definition
